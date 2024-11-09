a15845387e2e46d9af9de85ec3975f00cc761421
M Samarth Mahendra <39624187+SamarthMahendra@users.noreply.github.com>
2024-11-09 13:53:46 -0500

    Initial Commit

---
+++ AudioSyncServer/AudioSyncServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSyncServerApp.swift
+//  AudioSyncServer
+//
+//  Created by Samarth Mahendra on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioSyncServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioSyncServerTests/AudioSyncServerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSyncServerTests.swift
+//  AudioSyncServerTests
+//
+//  Created by Samarth Mahendra on 11/9/24.
+//
+
+import Testing
+@testable import AudioSyncServer
+
+struct AudioSyncServerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
